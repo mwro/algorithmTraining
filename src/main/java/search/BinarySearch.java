fb0016c021733e511b57e23c11f8d2374c84e687
Marcin <mwro91@gmail.com>
2019-05-18 22:03:34 +0200

    Added class body.

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -1,4 +1,33 @@
 package search;
 
 public class BinarySearch {
+
+    public static void main(String... args) {
+        int[] input = new int[]{1, 2, 16, 17, 70, 110, 207, 306, 400};
+        System.out.println(binarySearch(input, 306));
+    }
+
+    public static int binarySearch(int[] array, int item) {
+        int iterationCount = 0;
+
+        int lowIndex = 0;
+        int highIndex = array.length - 1;
+
+        while (lowIndex <= highIndex) {
+            iterationCount++;
+
+            int midIndex = (lowIndex + highIndex)/2;
+            int midValue = array[midIndex];
+            if (midValue == item) {
+                System.out.println("Completed search of array of size " + array.length + " in " + iterationCount + " steps.");
+                return midIndex;
+            } else if (midValue > item) {
+                highIndex = midIndex - 1;
+            } else {
+                lowIndex = midIndex + 1;
+            }
+        }
+        System.out.println("Completed search of array of size " + array.length + " in " + iterationCount + " steps.");
+        return -1;
+    }
 }
